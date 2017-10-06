1ea746225f97ad53f2f5cd64a976f39138189b17
Kevin Wu <kevin@kevinywu.com>
2017-10-06 03:36:12 -0500

    Initial compiler commit

--- kythera.js
+++ kythera.js
@@ -1,11 +1,15 @@
 const fs = require("fs")
 const Parser = require("./parser/Parser")
+const Compiler = require("./compiler/Compiler")
 
 try {
-	parser = new Parser(fs.readFileSync(process.argv[2]).toString())
-	program = parser.parse()
+	let parser = new Parser(fs.readFileSync(process.argv[2]).toString())
+	let program = parser.parse()
 
 	console.log(JSON.stringify(program, null, 2))
+
+	let compiler = new Compiler(program)
+	console.log(compiler.visitProgram())
 } catch(e) {
 	console.log(e)
 	return
