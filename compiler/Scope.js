8a13a79442e1c13d3b196a54fd42bc33cb54a4d8
Kevin Wu <kevin@kevinywu.com>
2017-10-08 04:13:59 -0500

    Add symbol table and assignment

---
+++ compiler/Scope.js
@@ -0,0 +1,54 @@
+
+// this largely comes from the Scope implementation in kevwu/kythera-antlr
+class Scope {
+	constructor(parent = null) {
+		// null for top-level scope
+		this.parent = parent
+		this.symbols = []
+
+		if(this.parent === null) {
+			this.level = 0
+		} else {
+			this.level = this.parent.level + 1
+		}
+	}
+
+	// initialize variable. Throws error if already declared.
+	create(name, type) {
+		// as long as the name is free in the current scope it can be used.
+		if(name in this.symbols) {
+			throw new Error(`${name} is already defined.`)
+		}
+
+		this.symbols[name] = type
+	}
+
+	// get type
+	get(name) {
+		// retrieve in local scope first
+		if(name in this.symbols) {
+			return this.symbols[name]
+		} else { // try parents
+			if(this.parent === null) {
+				throw new Error(`Undefined variable: ${name}`)
+			} else {
+				return this.parent.get(name)
+			}
+		}
+	}
+
+	// true if variable accessible from this scope or its parents, false otherwise
+	has(name) {
+		if(name in this.symbols) {
+			return true
+		} else {
+			if(this.parent === null) {
+				return false
+			} else {
+				return this.parent.has(name)
+			}
+		}
+	}
+}
+
+module.exports = Scope
