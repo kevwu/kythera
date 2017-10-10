cb162a38492c94de77611b3a3cdb02cf39dd3664
Kevin Wu <kevin@kevinywu.com>
2017-10-10 15:01:56 -0500

    Node type equivalency checker

--- compiler/Compiler.js
+++ compiler/Compiler.js
@@ -1,5 +1,37 @@
 Scope = require("./Scope")
 
+// for comparison with scope types
+const TYPES = {
+	int: {
+		type: "int"
+	},
+	float: {
+		type: "float"
+	},
+	bool: {
+		type: "bool",
+	},
+	str: {
+		type: "str"
+	},
+	"null": {
+		type: "null",
+	},
+	fn: {
+		type: "fn",
+		structure: {
+			returns: { // returns a null
+				type: "null",
+			},
+			parameters: [],
+		}
+	},
+	obj: {
+		type: "fn",
+		structure: []
+	}
+}
+
 class Compiler {
 	constructor(program) {
 		this.program = program
@@ -55,6 +87,12 @@ class Compiler {
 				} else {
 					throw new Error("bool literal used but value was not a boolean")
 				}
+			case "str":
+				if(typeof node.value === "string") {
+					return `new KYTHERA.value(${node.value}, "str")`
+				} else {
+					throw new Error("str literal used but value was not a string")
+				}
 			default:
 				throw new Error("Unhandled type: " + node.type)
 		}
@@ -69,8 +107,8 @@ class Compiler {
 		if(node.left.kind === "identifier") {
 			let lhsType = this.getNodeType(node.left)
 			let rhsType = this.getNodeType(node.right)
-			if(this.currentScope.get(node.left.name) !== rhsType) {
-				throw new Error(`Cannot assign ${rhsType} to ${node.left.name}, which is of type ${lhsType}`)
+			if(this.eqNodeType(this.currentScope.get(node.left.name), rhsType)) {
+				throw new Error(`Cannot assign ${rhsType.type} to ${node.left.name}, which is of type ${lhsType.type}`)
 			} else {
 				return `${node.left.name} = ${this.visitNode(node.right)}`
 			}
@@ -90,6 +128,42 @@ class Compiler {
 			default:
 				throw new Error(`Cannot find type for ${node.kind}`)
 		}
+	}
+
+	// compare two node types
+	eqNodeType(a, b) {
+		if(a.type !== b.type) {
+			return false
+		}
+
+		if(a.type === "fn") {
+			if(!this.eqNodeType(a.structure.returns, b.structure.returns)) {
+				return false
+			}
+
+			if(a.structure.parameters.length !== a.structure.parameters.length) {
+				return false
+			}
+
+			for(let i = 0; i < a.structure.parameters.length; i += 1) {
+				if(!this.eqNodeType(a.structure.parameters[i], b.structure.parameters[i])) {
+					return false
+				}
+			}
+			return true
+		}
+
+		if(a.type === "obj") {
+			if(Object.keys(a.structure).length !== Object.keys(b.structure).length) {
+				return false
+			}
+
+			if(Object.keys(a.structure).every((key, i) => this.eqNodeType(a.structure[key], b.structure[key]))) {
+				return false
+			}
+		}
+
+		return true
 	}
 }
 
