0618e8ae837fc908081946596d2ecabd4b858e7d
Kevin Wu <kevin@kevinywu.com>
2017-10-12 01:59:30 -0500

    Initializer for function literal

--- compiler/include.js
+++ compiler/include.js
@@ -2,12 +2,21 @@
 const KYTHERA = {
 	// equality test
 	equals: (a,b) => {
+		// TODO test structural equality
 		return a.value === b.value && a.type === b.type
 	},
 	value: class {
-		constructor(type, value) {
+		constructor(type, value, structure = null) {
 			this.value = value
 			this.type = type
+
+			if(type === "fn" || type === "obj") {
+				if(structure === null) {
+					throw new Error(`Cannot initialize ${type} without structure.`)
+				} else {
+					this.structure = structure
+				}
+			}
 		}
 	}
 }
