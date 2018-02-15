9e958f6f9e7ff69a3f3f88d6cf1dbbc26d58e4a0
Kevin Wu <kevin@kevinywu.com>
2018-02-14 23:05:53 -0600

    Combine literals test with primitives

--- test/primitives.parser.test.js
+++ test/primitives.parser.test.js
@@ -23,7 +23,7 @@ describe("Primitive Types", () => {
 		t("null")
 	})
 
-	describe("literals", () => {
+	describe("Literals", () => {
 		t("true")
 		t("false")
 		t("int")
@@ -32,7 +32,41 @@ describe("Primitive Types", () => {
 		t("null")
 	})
 
-	describe("casting", () => {
+	describe("Arithmetic", () => {
+		t("int Addition")
+		t("float addition")
+		t("subtraction")
+		t("division")
+		t("multiplication")
+		t("modulo")
+
+		describe("Operator precedence", () => {
+			t("multiply before addition")
+			t("parentheses")
+		})
+	})
+
+	describe("Boolean operations", () => {
+		t("negate false")
+		t("negate true")
+		t("and")
+		t("and")
+		t("short-circuit and")
+		t("or")
+		t("or")
+		t("short-circuit or")
+	})
+
+	describe("typeof", () => {
+		t("true")
+		t("false")
+		t("int")
+		t("float")
+		t("str")
+		t("null")
+	})
+
+	describe("Casting", () => {
 		describe("ints and floats", () => {
 			t("int as float")
 			t("float as int")
